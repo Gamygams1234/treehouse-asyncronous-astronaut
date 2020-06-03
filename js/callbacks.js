19cd20dfa41a5fe69579abf96699bf714cd40d08
Gamygams1234 <gamyburgos@gmail.com>
2020-06-02 19:58:40 -0700

    logging the outcome of the API

--- js/callbacks.js
+++ js/callbacks.js
@@ -1,14 +1,14 @@
-const astrosUrl = 'http://api.open-notify.org/astros.json';
-const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
-const peopleList = document.getElementById('people');
-const btn = document.querySelector('button');
+const astrosUrl = "http://api.open-notify.org/astros.json";
+const wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
+const peopleList = document.getElementById("people");
+const btn = document.querySelector("button");
 
 // Make an AJAX request
 function getJSON(url) {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
+  xhr.open("GET", url);
   xhr.onload = () => {
-    if(xhr.status === 200) {
+    if (xhr.status === 200) {
       let data = JSON.parse(xhr.responseText);
       console.log(data);
     }
@@ -18,7 +18,7 @@ function getJSON(url) {
 
 // Generate the markup for each profile
 function generateHTML(data) {
-  const section = document.createElement('section');
+  const section = document.createElement("section");
   peopleList.appendChild(section);
   section.innerHTML = `
     <img src=${data.thumbnail.source}>
@@ -27,3 +27,5 @@ function generateHTML(data) {
     <p>${data.extract}</p>
   `;
 }
+// this will log the data to the console after the button is clicked
+btn.addEventListener("click", () => getJSON(astrosUrl));
